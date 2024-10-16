a279fefcae7f17ba22cdd324e094b238a47aad82
shaps987 <davidmshaps@gmail.com>
2024-10-16 16:33:01 -0400

    Initial files for MakeCode project

---
+++ images.g.ts
@@ -0,0 +1,60 @@
+// Auto-generated code. Do not edit.
+namespace myImages {
+
+    helpers._registerFactory("image", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+            case "image1":
+            case "anvil":return img`
+.......bbbbbbbbbbbbbbbbbb.......
+......bddddddddddddddddddb......
+.....bddddddddddddddddddddb.....
+....bddddddddddddddddddddddb....
+...bddddddddddddddddddddddddb...
+..bddddddddddddddddddddddddddb..
+..c11111111111111111111111111c..
+..c11111111111111111111111111c..
+.bccccccccccccccccccccccccccccb.
+bb11111dd11dbbbbbbbbd11dd11111bb
+c11bbcc11dd11dbbbbd11dd11ccbb11c
+c1bbddbcb11dd111111dd11bcbddbb1c
+c1bbbddbdbd11dddddd11dbdbddbbb1c
+c11bbddcddbbd111111dbbddcddbb11c
+cb1111dcbd11bbbbbbbb11dbcd1111bc
+.cb111ccbdd1111111111ddbcc111bc.
+..cccc.cbdbb1bb11bb1bbdbc.cccc..
+.......cbdbb1db11bd1bbdbc.......
+.......cbdbd1dd11dd1dbdbc.......
+.......cbdbd1dd11dd1dbdbc.......
+......ccbdbd1dd11dd1dbdbcc......
+.....cbbbdbd1dd11dd1dbdbbbc.....
+.....cdbbdbd1dd11dd1dbdbbdc.....
+.....c11bbbd1dd11dd1dbbb11c.....
+.....cd111bbbbbbbbbbbb111dc.....
+....cccd1111111111111111dccc....
+...cbddbbb111111111111bbbddbc...
+..cbddddddbbbbbbbbbbbbddddddbc..
+..c11111111111111111111111111c..
+..c11111111111111111111111111c..
+..c11111111111111111111111111c..
+..cbbbbbbbbbbbbbbbbbbbbbbbbbbc..
+`;
+        }
+        return null;
+    })
+
+    helpers._registerFactory("animation", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+
+        }
+        return null;
+    })
+
+    helpers._registerFactory("song", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+
+        }
+        return null;
+    })
+
+}
+// Auto-generated code. Do not edit.
---
+++ tilemap.g.ts
@@ -0,0 +1,105 @@
+// Auto-generated code. Do not edit.
+namespace myTiles {
+    //% fixedInstance jres blockIdentity=images._tile
+    export const transparency16 = image.ofBuffer(hex``);
+
+    helpers._registerFactory("tilemap", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+            case "level1":
+            case "level1":return tiles.createTilemap(hex`0a0008000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000303030303030303030303030303030303030303`, img`
+. . . . . . . . . . 
+. . . . . . . . . . 
+. . . . . . . . . . 
+. . . . . . . . . . 
+. . . . . . . . . . 
+. . . . . . . . . . 
+2 2 2 2 2 2 2 2 2 2 
+2 2 2 2 2 2 2 2 2 2 
+`, [myTiles.transparency16,sprites.builtin.brick,sprites.builtin.oceanDepths0,sprites.builtin.oceanDepths3], TileScale.Sixteen);
+            case "level2":
+            case "level2":return tiles.createTilemap(hex`0a0008000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000303030303030303030303030303030303030303`, img`
+. . . . . . . . . . 
+. . . . . . . . . . 
+. . . . . . . . . . 
+. . . . . . . . . . 
+. . . . . . . . . . 
+. . . . . . . . . . 
+2 2 2 2 2 2 2 2 2 2 
+2 2 2 2 2 2 2 2 2 2 
+`, [myTiles.transparency16,sprites.builtin.brick,sprites.builtin.oceanDepths0,sprites.builtin.oceanDepths3], TileScale.Sixteen);
+            case "level3":
+            case "level3":return tiles.createTilemap(hex`0a0008000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000303030303030303030303030303030303030303`, img`
+. . . . . . . . . . 
+. . . . . . . . . . 
+. . . . . . . . . . 
+. . . . . . . . . . 
+. . . . . . . . . . 
+. . . . . . . . . . 
+2 2 2 2 2 2 2 2 2 2 
+2 2 2 2 2 2 2 2 2 2 
+`, [myTiles.transparency16,sprites.builtin.brick,sprites.builtin.oceanDepths0,sprites.builtin.oceanDepths3], TileScale.Sixteen);
+            case "level4":
+            case "level4":return tiles.createTilemap(hex`0a0008000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000303030303030303030303030303030303030303`, img`
+. . . . . . . . . . 
+. . . . . . . . . . 
+. . . . . . . . . . 
+. . . . . . . . . . 
+. . . . . . . . . . 
+. . . . . . . . . . 
+2 2 2 2 2 2 2 2 2 2 
+2 2 2 2 2 2 2 2 2 2 
+`, [myTiles.transparency16,sprites.builtin.brick,sprites.builtin.oceanDepths0,sprites.builtin.oceanDepths3], TileScale.Sixteen);
+            case "level5":
+            case "level5":return tiles.createTilemap(hex`1000100000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000`, img`
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+`, [myTiles.transparency16], TileScale.Sixteen);
+            case "level7":
+            case "level7":return tiles.createTilemap(hex`0a0008000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000303030303030303030303030303030303030303`, img`
+. . . . . . . . . . 
+. . . . . . . . . . 
+. . . . . . . . . . 
+. . . . . . . . . . 
+. . . . . . . . . . 
+. . . . . . . . . . 
+2 2 2 2 2 2 2 2 2 2 
+2 2 2 2 2 2 2 2 2 2 
+`, [myTiles.transparency16,sprites.builtin.brick,sprites.builtin.oceanDepths0,sprites.builtin.oceanDepths3], TileScale.Sixteen);
+            case "background":
+            case "level6":return tiles.createTilemap(hex`0a0008000202020202020202020203040304030403040304050605060506050605060304030403040304030405060506050605060506080808080808080808080101010101010101010107070707070707070707`, img`
+. . . . . . . . . . 
+. . . . . . . . . . 
+. . . . . . . . . . 
+. . . . . . . . . . 
+. . . . . . . . . . 
+. . . . . . . . . . 
+2 2 2 2 2 2 2 2 2 2 
+2 2 2 2 2 2 2 2 2 2 
+`, [myTiles.transparency16,sprites.dungeon.greenOuterNorth0,sprites.dungeon.purpleOuterNorth0,sprites.dungeon.purpleInnerNorthWest,sprites.dungeon.purpleInnerNorthEast,sprites.dungeon.purpleInnerSouthWest,sprites.dungeon.purpleInnerSouthEast,sprites.dungeon.greenOuterSouth0,sprites.dungeon.purpleOuterSouth1], TileScale.Sixteen);
+        }
+        return null;
+    })
+
+    helpers._registerFactory("tile", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+            case "transparency16":return transparency16;
+        }
+        return null;
+    })
+
+}
+// Auto-generated code. Do not edit.
